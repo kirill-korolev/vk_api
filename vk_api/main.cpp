7f8738da538778e52ab128e16a8599297347b755
kirill-korolev <kafafyf@gmail.com>
2018-01-16 01:15:56 +0300

    Initial Commit

---
+++ vk_api/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vk_api
+//
+//  Created by Kirill Korolev on 16/01/2018.
+//  Copyright © 2018 Kirill Korolev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
