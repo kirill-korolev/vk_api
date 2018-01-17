167667eb0406a5877d56fe594d72be69e2f2b478
kirill-korolev <kafafyf@gmail.com>
2018-01-17 17:27:31 +0300

    main window update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
+#include "urlrequestinterceptor.h"
+#include <QWebEngineProfile>
 #include <QApplication>
+
+#include "vk_api/vk_api_utility.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QWebEngineUrlRequestInterceptor* interceptor = new UrlRequestInterceptor(&w);
+    QWebEngineProfile::defaultProfile()->setRequestInterceptor(interceptor);
 
     return a.exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,15 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    vk_api::auth("6334044", vk_api::scopes::friends, [](const std::string& c) -> std::string{
+    vk_api::auth<>("6334044", vk_api::scopes::friends, [this](const std::string& contents) -> std::string {
+        this->ui->m_webview->setHtml(QString(contents.c_str()));
+
         return "";
     });
+
 }
 
 MainWindow::~MainWindow()
